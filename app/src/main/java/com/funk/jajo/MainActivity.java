1bd5c761665675e9d8a2299b68e09f9828587a5e
JosFun <joshua.funk@tu-ilmenau.de>
2020-04-12 09:29:13 +0200

    Storing data on the device permanently works for now.

--- app/src/main/java/com/funk/jajo/MainActivity.java
+++ app/src/main/java/com/funk/jajo/MainActivity.java
@@ -2,6 +2,7 @@ package com.funk.jajo;
 
 import android.arch.lifecycle.ViewModelProvider;
 import android.arch.lifecycle.ViewModelProviders;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
@@ -10,13 +11,48 @@ import android.view.MenuItem;
 import android.widget.TextView;
 
 import com.funk.jajo.customtypes.Person;
+import com.funk.jajo.customtypes.PersonStorer;
 
 public class MainActivity extends AppBarActivity {
+    private static final String FIRST_SUFFIX = "_FIRST";
+    private static final String SECOND_SUFFIX = "_SECOND";
 
     private AusgabenFragment ausgabenFragment;
     private EinkaufslisteFragment listenFragment;
     private AppViewModel viewModel;
 
+    private void loadPersonData ( ) {
+        SharedPreferences sP = this.getSharedPreferences( getString(R.string.PERSON_SHARED_PREF), MODE_PRIVATE);
+        String loadKey1 = getString ( R.string.PERSON_KEY ) + FIRST_SUFFIX;
+        String loadKey2 = getString ( R.string.PERSON_KEY ) + SECOND_SUFFIX;
+
+        PersonStorer ps1 = new PersonStorer( loadKey1, sP);
+        PersonStorer ps2 = new PersonStorer( loadKey2, sP );
+
+        Person p1 = ps1.loadPerson();
+        Person p2 = ps2.loadPerson();
+
+        if ( this.viewModel == null ) {
+            this.viewModel = ViewModelProviders.of ( this ).get( AppViewModel.class );
+        }
+
+        if ( ps1 != null && ps2 != null ) {
+            this.viewModel.setFirst( p1 );
+            this.viewModel.setSecond( p2 );
+        }
+
+    }
+
+    private void storePersonData ( ) {
+        if ( this.viewModel != null && this.viewModel.getSecond() != null && this.viewModel.getSecond() != null ) {
+            SharedPreferences sP = this.getSharedPreferences( getString(R.string.PERSON_SHARED_PREF), MODE_PRIVATE );
+            PersonStorer pS1 = new PersonStorer( this.viewModel.getFirst(),
+                    getString(R.string.PERSON_KEY )+ FIRST_SUFFIX, sP);
+            PersonStorer pS2 = new PersonStorer( this.viewModel.getSecond(),
+                    getString(R.string.PERSON_KEY) + SECOND_SUFFIX, sP);
+        }
+
+    }
     private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
             = new BottomNavigationView.OnNavigationItemSelectedListener() {
 
@@ -52,6 +88,9 @@ public class MainActivity extends AppBarActivity {
         this.viewModel = ViewModelProviders.of ( this ).get( AppViewModel.class );
         this.viewModel.setCurrentFragment( FragmentType.AUSGABEN );
 
+        /* Try to load existing persons from the phone's storage */
+        this.loadPersonData();
+
         if ( this.viewModel.getFirst() == null && this.viewModel.getSecond() == null ) {
             /* Create the two persons that use this app in order to share their payments with each other. */
             this.viewModel.setFirst( new Person( getString ( R.string.first)));
@@ -74,6 +113,13 @@ public class MainActivity extends AppBarActivity {
     protected void createFragments() {
         this.ausgabenFragment = new AusgabenFragment();
         this.listenFragment = new EinkaufslisteFragment();
+    }
+
+    @Override
+    public void onPause ( ) {
+        super.onPause();
+        /* Store the Persons on the phone. */
+        this.storePersonData();
     }
 
 }
