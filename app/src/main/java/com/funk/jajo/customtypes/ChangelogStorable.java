aa16d19d96e6f3e6404b76105057cf8928964f70
JosFun <joshua.funk@tu-ilmenau.de>
2020-08-16 08:15:44 +0200

    Added a method to ChangelogStorable that is used to attain access to the LocalChangelog of the device.

--- app/src/main/java/com/funk/jajo/customtypes/ChangelogStorable.java
+++ app/src/main/java/com/funk/jajo/customtypes/ChangelogStorable.java
@@ -10,24 +10,42 @@ public class ChangelogStorable {
     private Changelog firstChangelog;
     private Changelog secondChangelog;
 
-    public ChangelogStorable ( Changelog first, Changelog second ) {
+    public ChangelogStorable(Changelog first, Changelog second) {
         this.firstChangelog = first;
         this.secondChangelog = second;
     }
 
     /**
      * Get access to the changelog of the remote device that is being stored online
+     *
      * @param deviceName This device's name
      * @return The remote device's changelog
+     * @throws InvalidParameterException
      */
-    public Changelog getRemoteChangelog ( String deviceName ) throws InvalidParameterException {
-        if ( !this.firstChangelog.getOriginHost().equals( deviceName ) &&
-                this.secondChangelog.getOriginHost().equals( deviceName )) {
+    public Changelog getRemoteChangelog(String deviceName) throws InvalidParameterException {
+        if (!this.firstChangelog.getOriginHost().equals(deviceName) &&
+                this.secondChangelog.getOriginHost().equals(deviceName)) {
             return this.firstChangelog;
-        } else if ( !this.secondChangelog.getOriginHost().equals( deviceName ) &&
-                this.firstChangelog.getOriginHost().equals( deviceName)) {
+        } else if (!this.secondChangelog.getOriginHost().equals(deviceName) &&
+                this.firstChangelog.getOriginHost().equals(deviceName)) {
             return this.secondChangelog;
-        }
-        else throw new InvalidParameterException();
+        } else throw new InvalidParameterException();
+    }
+
+    /**
+     * Get access to the changelog of the local device that is supposedly being stored on the
+     * local device
+     * @param deviceName This device's name
+     * @return The local device's changelog
+     * @throws InvalidParameterException
+     */
+    public Changelog getLocalChangelog(String deviceName) throws InvalidParameterException {
+        if (this.firstChangelog.getOriginHost().equals(deviceName) &&
+                !this.secondChangelog.getOriginHost().equals(deviceName)) {
+            return this.firstChangelog;
+        } else if (this.secondChangelog.getOriginHost().equals(deviceName) &&
+                this.firstChangelog.getOriginHost().equals(deviceName)) {
+            return this.secondChangelog;
+        } else throw new InvalidParameterException();
     }
 }
